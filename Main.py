b0c057bf4436e3ce11ddfda746aded36150b4017
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-01 19:09:31 -0400

    Day 5 Imports added, also adding time module Put Todo_Functions in its own folder

--- Main.py
+++ Main.py
@@ -1,26 +1,24 @@
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+from modules import Todo_Functions  
+import time 
 
+current_time = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", current_time)
 
 while True:
     user_action = input('Type add,show, edit, complete or exit: ')
     user_action = user_action.strip()
-    
+       
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = Todo_Functions.get_todos()
                 
         todos.append(todo + "\n")
-            
-        with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-            
+        
+        Todo_Functions.write_todos(todos)
     elif user_action.startswith("show"):
         
-        todos = get_todos()
+        todos = Todo_Functions.get_todos()
                 
         for index, items in  enumerate(todos):
                 items = items.strip('\n')
@@ -32,13 +30,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1 
                 
-            todos = get_todos()
+            todos = Todo_Functions.get_todos()
             
             new_todo = input('Enter a new Todo: ')
             todos[number] = new_todo +'\n'
-                
-            with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
+            
+            Todo_Functions.write_todos(todos)      
         except ValueError:
             print("Your command is not Valid.")
             continue  
@@ -50,24 +47,22 @@ while True:
         try:    
             number = int(user_action[9:])
                 
-            todos = get_todos()
+            todos = Todo_Functions.get_todos()
                 
             index = number - 1
             removed_todo = todos[index].strip('\n')
             todos.pop(index)
-        
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-                    
-                message = f'Todo {removed_todo} was completed and removed from the list.'
-                print(message)
+            
+            Todo_Functions.write_todos(todos) 
+            
+            message = f'Todo {removed_todo} was completed and removed from the list.'
+            print(message)
         except IndexError:
             print("There is no item with that number.")
             continue
         except ValueError:
             print("Your command is not Valid.")
             continue        
-            
     elif user_action.startswith("exit"):
         break
     else:
