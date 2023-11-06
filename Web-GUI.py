a3d542f57fa73cb9aaba02674ee61776ac40f7c6
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-06 02:47:29 -0500

    Using Streamlit to make our Todo App A web based program

--- Web-GUI.py
+++ Web-GUI.py
@@ -1,18 +1,25 @@
 import streamlit as st
 from Modules import Todo_Functions
 
-
 todos = Todo_Functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    Todo_Functions.write_todos(todos)
+    
 
 st.title("My Todo App")
 st.subheader("Welcome to your Web Based To do app!")
 #st.write("hello funded trader!")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    st.checkbox(todo, key=f"todo_{index}")
     
-st.text_input(label="", placeholder="Enter a Todo!", label_visibility="hidden")
+    
+st.text_input(label="", placeholder="Enter a Todo!", label_visibility="hidden",
+              on_change=add_todo, key="new_todo")
 
 
 
